83b7b168176aea5ed4ef0754a41b850c8ffff94d
LucasJ-22 <Lucasjcc3020@gmail.com>
2020-04-17 14:36:44 -0300

    SandGrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'], #app165772027@heroku.com
-    :password       => ENV['SENDGRID_PASSWORD'], #z5c5qhd36164
+    :user_name      => ENV['SENDGRID_USERNAME'], 
+    :password       => ENV['SENDGRID_PASSWORD'], 
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
