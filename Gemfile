6e84c2a667d386b5ecb45eb290f2cf91a682ba3c
LucasJ-22 <Lucasjcc3020@gmail.com>
2020-04-17 14:32:00 -0300

    SandGrid config

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.3'
 
 
+#Sandgrid
+gem 'sendgrid-ruby'
 #DB  
 gem 'pg', '1.1.4'
 # Make feke users
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,10 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://sam-ple.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
+  host = 'sam-ple.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host protocol: 'https' }
+  ActionMailer::Base.smtp_settings = 
+  {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
