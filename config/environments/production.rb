4208ce6490e7fac6110df1f7a34ee7100519c622
LucasJ-22 <Lucasjcc3020@gmail.com>
2020-04-17 14:47:55 -0300

    SandGrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sam-ple.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host protocol: 'https' }
+  host = 'https://sam-ple.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = 
   {
     :address        => 'smtp.sendgrid.net',
