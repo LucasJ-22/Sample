1d08461130247005b4693f5fc3c3dc2bb16c35bd
LucasJ-22 <Lucasjcc3020@gmail.com>
2020-04-29 15:20:07 -0300

    Heroku Fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
-    else
+    else 
       render 'new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -37,9 +37,9 @@ class User < ApplicationRecord
 
     # Activates an account.
     def activate
-        #update_columns(activated: true, activated_at: Time.zone.now)
-        update_attribute(:activated,true)
-        update_attribute(:activated_at, Time.zone.now)
+        update_columns(activated: true, activated_at: Time.zone.now)
+        #update_attribute(:activated,true)
+        #update_attribute(:activated_at, Time.zone.now)
     end
 
     # Sends activation email.
@@ -64,13 +64,13 @@ class User < ApplicationRecord
 
     private
 
-        def downcase_email
-            self.email = email.downcase
-        end
+    def downcase_email
+        self.email = email.downcase
+    end
 
-        def create_activation_digest
-            self.activation_token  = User.new_token
-            self.activation_digest = User.digest(activation_token)
-        end
+    def create_activation_digest
+        self.activation_token  = User.new_token
+        self.activation_digest = User.digest(activation_token)
+    end
 end
  
