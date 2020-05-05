c395fd2008137b8256a4e8a8d302da57f64c72cf
LucasJ-22 <Lucasjcc3020@gmail.com>
2020-05-05 11:55:31 -0300

    Branch fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,26 +53,26 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
-
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-  end
 
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   get '/about',     to: 'static_pages#about'
   get '/contact',   to: 'static_pages#contact'
   get '/signup',    to: 'users#new'
-  get 'password_resets/new'
-  get 'password_resets/edit'
   get '/login',     to: 'sessions#new'
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
