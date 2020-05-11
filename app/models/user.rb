1babdd7d0d3526629473c2d457564b819223c4fc
LucasJ-22 <Lucasjcc3020@gmail.com>
2020-05-11 15:00:50 -0300

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,8 +76,8 @@ class User < ApplicationRecord
     end
 
     def feed
-        following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
-        Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+        part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+        Micropost.joins(user: :followers).where(part_of_feed, { id: id })
     end
 
     def follow(other_user)
