d91fcb0559eb6cdb48a6bf2dbb7dd63e3c18fc04
LucasJ-22 <Lucasjcc3020@gmail.com>
2020-05-11 18:13:07 -0300

    Add user following

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -13,7 +13,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_not @user.following.empty?
     assert_match @user.following.count.to_s, response.body
     @user.following.each do |user|
-      assert_select "a[href=?]", user_path(user)
+      assert_select "a[href=?]", user_path(@user)
     end
   end
 
@@ -22,7 +22,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_not @user.followers.empty?
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
-      assert_select "a[href=?]", user_path(user)
+      assert_select "a[href=?]", user_path(@user)
     end
   end
 
